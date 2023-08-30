e1e01034479ed6231e6c1fba7df779cc1e180954
Olim2508 <rahmatovolim3@gmail.com>
2023-08-30 21:27:56 +0500

    created routes constant, updated app js routing

---
+++ src/constants/routes.js
@@ -0,0 +1,10 @@
+export default Object.freeze({
+  home: '/',
+  blogCreate: '/create',
+  blogDetail: '/blogs/:id',
+  blogUpdate: '/update/:id',
+  signUp: '/sign-up/',
+  logIn: '/log-in/',
+  categories: '/categories/',
+  categoryCreate: '/category/create',
+});
